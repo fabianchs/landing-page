caf8a95044533a52bd2a3772526dec1fc2a71042
fabianchs <fabichasola@hotmail.com>
2021-03-01 02:29:01 +0000

    avance

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,9 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+//import { Navbar } from "./component/navbar.js";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+//React.Dom.render(<Navbar />, document.querySelector("#app"));
